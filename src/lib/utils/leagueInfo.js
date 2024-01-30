2e70378a47649c151a729f7d2c42d78d15419a45
phat-mike <michaelpdonahuejr@gmail.com>
2024-01-30 00:46:07 -0500

    Update manager's photo in leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -47,7 +47,7 @@ export const managers = [
       "name": "Phat Mike",
       "location": "Detroit", // (optional)
       "bio": "It's not a nursing home! It's a retirement community.",
-      "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/question.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2019, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "phl", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Dynasty", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
