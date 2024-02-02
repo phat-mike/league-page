e7919ab8a281b1f7c5a80847be15da931eebc702
phat-mike <michaelpdonahuejr@gmail.com>
2024-02-01 19:24:59 -0500

    Commented out unused import statement

--- src/lib/utils/helper.js
+++ src/lib/utils/helper.js
@@ -17,7 +17,7 @@ import { predictScores } from './helperFunctions/predictOptimalScore';
 import { getBrackets } from './helperFunctions/leagueBrackets';
 import { getBlogPosts, generateParagraph } from './helperFunctions/getBlogPosts';
 import { getLeagueStandings } from './helperFunctions/leagueStandings';
-import { getGoogDocEmbedLink } from './helperFunctions/getGoogDocConst';
+//import { getGoogDocEmbedLink } from './helperFunctions/getGoogDocConst';
 
 export {
     enableBlog,
@@ -55,5 +55,5 @@ export {
     parseDate,
     getAvatar,
     getTeamFromTeamManagers,
-    getGoogDocEmbedLink,
+    //getGoogDocEmbedLink,
 }
